a74e3baf09af757e1a1eb4c82ac859a21ff47150
Emiliano Cervantes <ovman96@hotmail.com>
2018-02-04 16:49:02 -0600

    Register screen and methods in Login Activity.

--- app/src/main/java/com/example/emilianocervantes/tindertec/MainActivity.java
+++ app/src/main/java/com/example/emilianocervantes/tindertec/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.emilianocervantes.tindertec;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+    private EditText correo, contrasena;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        correo = (EditText)findViewById(R.id.userEmail);
+        contrasena = (EditText)findViewById(R.id.userPassword);
+    }
+
+    public void iniciarSesion(View view){
+
+    }
+
+    public void irRegistrarse(View view){
+        Intent intent = new Intent(this, RegistroActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
